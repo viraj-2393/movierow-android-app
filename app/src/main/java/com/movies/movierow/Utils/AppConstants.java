f1315e8dcd1e37a272a2d01480b28628e56aba9a
Viraj Kumar Singh <58391492+viraj-2393@users.noreply.github.com>
2022-03-11 11:31:16 +0530

    Update AppConstants.java

--- app/src/main/java/com/movies/movierow/Utils/AppConstants.java
+++ app/src/main/java/com/movies/movierow/Utils/AppConstants.java
@@ -1,8 +1,8 @@
 package com.movies.movierow.Utils;
 
 public class AppConstants {
-    public static String YoutubeApiKey = "AIzaSyC75MHdN31xj0EJS-F90e-PhNVWJZH8MwU";
-    public static String TMDB_API_KEY = "c09b417853b21948266e85cb76df8365";
+    public static String YoutubeApiKey = "";//YOUR YOUTUBE API KEY
+    public static String TMDB_API_KEY = "";//YOUR TMDB API KEY
     public static String Trailer_URL = "https://youtube.googleapis.com/youtube/v3/";
     public static String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
 }
